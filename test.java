73c834fd2f222d8f0fbca6a7ac0b265d44239f2b
quang2652001 <65891056+quang2652001@users.noreply.github.com>
2023-01-29 23:15:26 +0700

    Update test.java

--- test.java
+++ test.java
@@ -1,5 +1,6 @@
-public class Question2 {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-    }
+
+public class main {
+	public static void main(String[] args) {
+		System.out.println("Hello world"); 
+	}
 }
