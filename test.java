a7f42d4e03899028686cee77c536e0cc9f7765e3
quang2652001 <65891056+quang2652001@users.noreply.github.com>
2023-01-29 23:03:45 +0700

    Create test.java

---
+++ test.java
@@ -0,0 +1,15 @@
+public class Question2 {
+    public static void main(String[] args) {
+        Triangle trial1 = new Triangle(1,2,1);
+        trial1.verifyTriangle();
+
+        Triangle trial2 = new Triangle(2,2,2);
+        trial2.verifyTriangle();
+
+        Triangle trial3 = new Triangle(2,2,3);
+        trial3.verifyTriangle();
+
+        Triangle trial4 = new Triangle(3,4,5);
+        trial4.verifyTriangle();
+    }
+}
